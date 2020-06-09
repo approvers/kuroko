365cba96112b8a18195edc2c561f78d47b277f5c
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-06-09 21:01:39 +0900

    重複箇所をマクロに置き換え

--- src/messages.rs
+++ src/messages.rs
@@ -1,11 +1,14 @@
-pub const NO_DISCORD_TOKEN: &str = "おﾈｪｻﾏｰｰｰｯｯｯｯｯ!!!!! トークンがございませんのｯｯｯｯｯｯｯｯ!!!!!!";
-pub const DISCORD_LAUNCH_FAILED: &str =
-    "おﾈｪｻﾏｰｰｰｯｯｯｯｯ!!!!! Discordに接続できませんのｯｯｯｯｯｯｯｯ!!!!!!";
-pub const READY: &str = "おﾈｪｻﾏｰｰｰｯｯｯｯｯ!!!!! 準備完了ですのｯｯｯｯｯｯｯｯ!!!!!!";
+macro_rules! kuroko {
+    ($e:expr) => {concat!("おﾈｪｻﾏｰｰｰｯｯｯｯｯ!!!!!", $e, "ｯｯｯｯｯｯｯｯ!!!!!!")}
+}
+
+pub const NO_DISCORD_TOKEN: &str = kuroko!("トークンがございませんの");
+pub const DISCORD_LAUNCH_FAILED: &str = kuroko!("Discordに接続できませんの");
+pub const READY: &str = kuroko!("準備完了ですの");
 pub const JUDGEMENT: &str = "ジャッジメントですのっ！";
-pub const MESSAGE_SEND_FAIL: &str = "おﾈｪｻﾏｰｰｰｯｯｯｯｯ!!!!! 発言出来ませんのｯｯｯｯｯｯｯ!!!!!";
-pub const NOT_ENOUGH_ARGUMENTS: &str = "おﾈｪｻﾏｰｰｰｯｯｯｯｯ!!!!! 引数が足りませんのｯｯｯｯｯｯ!!!!!!";
-pub const TOO_LONG: &str = "おﾈｪｻﾏｰｰｰｯｯｯｯｯ!!!!! メッセージが長すぎて送信できませんのｯｯｯｯｯｯ!!!!!";
+pub const MESSAGE_SEND_FAIL: &str = kuroko!("発言できませんの");
+pub const NOT_ENOUGH_ARGUMENTS: &str = kuroko!("引数が足りませんの");
+pub const TOO_LONG: &str = kuroko!("メッセージが長すぎて送信できませんの");
 
 #[allow(non_snake_case)]
 pub fn ITS_THE_TIME(text: &str, daggered: bool) -> String {
